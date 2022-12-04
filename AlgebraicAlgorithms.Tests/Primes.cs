ed28838536175b4649a260f65c4fae5d64b1a51e
Igor Teplych <igorvrt@mail.ru>
2022-12-04 18:54:53 +0600

    Добавил алгоритм поиска простых чисел с оптимизациями поиска и делением только на простые числа, O(N * Sqrt(N) / Ln (N)) и тесты для него.

---
+++ AlgebraicAlgorithms.Tests/Primes.cs
@@ -0,0 +1,93 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AlgebraicAlgorithms.Tests
+{
+    public class Primes
+    {
+        [SetUp]
+        public void Setup()
+        {
+
+        }
+
+        [Test]
+        public void Test0()
+        {
+            Assert.AreEqual(AlgebraicAlgorithms.Primes.GetCountPrime(10), 4);
+        }
+        [Test]
+        public void Test1()
+        {
+            Assert.AreEqual(AlgebraicAlgorithms.Primes.GetCountPrime(1), 0);
+        }
+        [Test]
+        public void Test2()
+        {
+            Assert.AreEqual(AlgebraicAlgorithms.Primes.GetCountPrime(2), 1);
+        }
+        [Test]
+        public void Test3()
+        {
+            Assert.AreEqual(AlgebraicAlgorithms.Primes.GetCountPrime(3), 2);
+        }
+        [Test]
+        public void Test4()
+        {
+            Assert.AreEqual(AlgebraicAlgorithms.Primes.GetCountPrime(4), 2);
+        }
+        [Test]
+        public void Test5()
+        {
+            Assert.AreEqual(AlgebraicAlgorithms.Primes.GetCountPrime(5), 3);
+        }
+        [Test]
+        public void Test6()
+        {
+            Assert.AreEqual(AlgebraicAlgorithms.Primes.GetCountPrime(100), 25);
+        }
+        [Test]
+        public void Test7()
+        {
+            Assert.AreEqual(AlgebraicAlgorithms.Primes.GetCountPrime(1000), 168);
+        }
+        [Test]
+        public void Test8()
+        {
+            Assert.AreEqual(AlgebraicAlgorithms.Primes.GetCountPrime(10000), 1229);
+        }
+        [Test]
+        public void Test9()
+        {
+            Assert.AreEqual(AlgebraicAlgorithms.Primes.GetCountPrime(100000), 9592);
+        }
+        [Test]
+        public void Test10()
+        {
+            Assert.AreEqual(AlgebraicAlgorithms.Primes.GetCountPrime(1000000), 78498);
+        }
+        [Test]
+        public void Test11()
+        {
+            Assert.AreEqual(AlgebraicAlgorithms.Primes.GetCountPrime(10000000), 664579);
+        }
+        [Test]
+        public void Test12()
+        {
+            Assert.AreEqual(AlgebraicAlgorithms.Primes.GetCountPrime(100000000), 5761455);
+        }
+        //[Test]
+        public void Test13()
+        {
+            Assert.AreEqual(AlgebraicAlgorithms.Primes.GetCountPrime(1000000000), 50847534);
+        }
+        [Test]
+        public void Test14()
+        {
+            Assert.AreEqual(AlgebraicAlgorithms.Primes.GetCountPrime(123456789), 7027260);
+        }
+    }
+}
---
+++ AlgebraicAlgorithms/Primes.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AlgebraicAlgorithms
+{
+    public static class Primes
+    {
+        /// <summary>
+        /// Алгоритм поиска простых чисел с оптимизациями поиска и делением только на простые числа, O(N * Sqrt(N) / Ln (N))
+        /// </summary>
+        /// <returns></returns>
+        public static long GetCountPrime(long n)
+        {
+            if (n < 2)
+                return 0;
+
+            long[] primes = new long[(n/2) + 1];
+            primes[0] = 2;
+
+            long number = 3;
+            long count = 1;
+            while (number <= n)
+            {
+                if (IsPrime(number, primes))
+                {
+                    primes[count++] = number;
+                }
+                number++;
+            }
+            return count;
+        }
+
+        static bool IsPrime(long n, long[] primes)
+        {
+            var sqrt = Math.Sqrt(n);
+
+            long count = 0;
+            while (primes[count] <= sqrt)
+            {
+                if(n % primes[count++] == 0)
+                {
+                    return false;
+                }
+            }
+            return true;
+        }
+    }
+}
